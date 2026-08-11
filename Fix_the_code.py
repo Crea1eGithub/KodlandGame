cb81bfa9a14e018f24c77207c0cba5543dc0e831
Crea1eGithub <Crea1e_PI@outlook.com>
2026-08-10 21:26:41 -0500

    Add files via upload

---
+++ Fix_the_code.py
@@ -0,0 +1,137 @@
+import time
+
+try:
+    import shelve
+
+    storage = shelve.open("local_storage")
+    username = storage.get("username", "")
+    on_skulpt = False
+    print()
+except Exception:
+    username = ""
+    on_skulpt = True
+    print("(parece que no puedes usar la libreria shelve y/o estas en skulpt)")
+    time.sleep(1)
+
+
+def wait(seconds):
+    time.sleep(seconds)
+code_works = "ok, vamos configurando el trazo"
+
+def validate_command(broken_code, correct_command1, correct_command2=""):
+    print()
+    user_code = input(
+        '"'
+        + broken_code
+        + '"         #pon el codigo bien escrito en el input que puse, este no me funciona'
+    )
+    print("hmm...")
+    time.sleep(2)
+
+    if correct_command2 == "":
+        if user_code == correct_command1:
+            print(code_works)
+            return True
+    else:
+        if user_code == correct_command1 or user_code == correct_command2:
+            print(code_works)
+            return True
+
+    print(
+        "hmm, parece que escribiste algo mal, yo creo que debes cerrar y abrir el programa para reiniciarlo"
+    )
+    raise SystemExit
+
+
+print("Hola AVENTURERO, bienvenido al juego!")
+wait(1)
+print("Cargando[.      ]")
+wait(1)
+print("Cargando[..     ]")
+wait(1)
+print("Cargando[...    ]")
+wait(3)
+print("emm")
+wait(1)
+print("porq no carga? XD")
+wait(1)
+print("Reintentando carga...")
+wait(3)
+print("ERROR: bad input in ALL CODE")
+wait(2)
+print("Recorcholis, no sabia que mi codigo estaba tan mal")
+wait(1)
+print("me ayudas a arreglarlo? :V")
+wait(2)
+print("(si)")
+wait(2)
+
+if username == "":
+    print("Gracias, dime tu nombre para que comencemos!")
+    username = input()
+    if username == "":
+        print("hmm, te llamare aventurero anonimo")
+        username = "Aventurero Anonimo"
+    if not on_skulpt:
+        storage["username"] = username
+        print("(Ya dijiste que es ", username, ")")
+    print("tu nombre es ", username, "?")
+    wait(2)
+    print("(si)")
+    print("Gracias", username, "!")
+else:
+    print("...")
+    wait(1)
+    print("¿Como es que aparece que ya te pregunte tu nombre?")
+    wait(1)
+    print(
+        "Ohh verdad que tu eres",
+        username,
+        ", se me olvido por un momento, no terminaban de cargar las librerias",
+    )
+
+if not on_skulpt:
+    storage.close()
+
+wait(0.5)
+print()
+wait(1)
+
+import turtle
+
+t = turtle.Turtle()
+
+
+validate_command("t.fil('1')", "t.fill(1)")
+t.fill(1)
+validate_command(
+    "t.colorlapiz('green')", "t.pencolor('green')", 't.pencolor("green")'
+)
+t.pencolor("green")
+wait(2)
+code_works = "buen trabajo, ya vas dibujando el cre- ah verdad que no debo spoilear"
+validate_command("t.forward", "t.forward(100)", "t.fd(100)")
+t.forward(100)
+wait(2)
+validate_command("t.lefet(90)", "t.left(90)", "t.lt(90)")
+t.left(90)
+wait(2)
+print("Vas bien,", username, ", no lo niego")
+wait(2)
+validate_command("t.forward", "t.forward(100)", "t.fd(100)")
+t.forward(100)
+wait(2)
+validate_command("t.lefet(90)", "t.left(90)", "t.lt(90)")
+t.left(90)
+wait(2)
+validate_command("t.forward", "t.forward(100)", "t.fd(100)")
+t.forward(100)
+wait(2)
+validate_command("t.lefet(90)", "t.left(90)", "t.lt(90)")
+t.left(90)
+wait(2)
+validate_command("t.forward", "t.forward(100)", "t.fd(100)")
+t.forward(100)
+wait(2)
+
+print("[SISTEMA]: Contorno de la cabeza completado con éxito.")
